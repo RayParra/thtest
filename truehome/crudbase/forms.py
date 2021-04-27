45fb98f839be20f6eb06690465bf9c9e4711a110
RayParra <rparragalaviz@gmail.com>
2021-04-26 19:49:18 -0700

    property_id forms validation in activity forms

--- truehome/crudbase/forms.py
+++ truehome/crudbase/forms.py
@@ -2,12 +2,19 @@ from django import forms
 from django.forms import ValidationError
 from django.utils import timezone
 from .validators import CheckDate
-from api.models import Activity
+from api.models import Activity, Property
 
 class CreateActivityForm(forms.ModelForm):
     schedule = forms.DateTimeField(required=False, widget=forms.TextInput(attrs={"type": "date"}))
-    #property_id = forms.ChoiceField(choices=[(property_id.id, choice) for choice in Activity.objects.filter(status="active")])
-    #property_id = forms.ModelChoiceField(queryset=Activity.objects.filter(status="active"))
+    #property_id = forms.ChoiceField(choices=[(property_id.id, property_id) for property_id in Property.objects.filter(status="active")])
+    property_id = forms.ModelChoiceField(queryset=Property.objects.filter(status="active"))
+    
+    def clean_property_id(self):
+        property_id = self.cleaned_data["property_id"]
+        prop_id = Property.objects.filter(id=property_id.id).first()
+        print(property_id)
+        print(type(prop_id))
+        return property_id
     
     def clean_schedule(self):
         schedule = self.cleaned_data["schedule"]
